9a1fc25a12cffb686721acf5f6fd381f1ca07d45
宋瑞 <songrui@vip56.cn>
2020-05-27 17:31:31 +0800

    first commit

---
+++ SwaggerUI/Controllers/TestSwaggerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SwaggerUI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestSwaggerController : ControllerBase
+    {
+        [HttpGet("test")]
+        public OkObjectResult testSwapper()
+        {
+            return Ok("success");
+        }
+
+    }
+}
---
+++ SwaggerUI/Options/EnumDocumentFilter.cs
@@ -0,0 +1,64 @@
+﻿using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace Sino.SlaveNode.Web
+{
+    /// <summary>
+    /// Add enum value descriptions to Swagger
+    /// </summary>
+    public class EnumDocumentFilter : IDocumentFilter
+    {
+        /// <inheritdoc />
+        public void Apply(SwaggerDocument swaggerDoc, DocumentFilterContext context)
+        {
+            // add enum descriptions to result models
+            foreach (var schemaDictionaryItem in swaggerDoc.Definitions)
+            {
+                var schema = schemaDictionaryItem.Value;
+                foreach (var propertyDictionaryItem in schema.Properties)
+                {
+                    var property = propertyDictionaryItem.Value;
+                    var propertyEnums = property.Enum;
+                    if (propertyEnums != null && propertyEnums.Count > 0)
+                    {
+                        property.Description += DescribeEnum(propertyEnums);
+                    }
+                }
+            }
+        }
+
+        private static string DescribeEnum(IEnumerable<object> enums)
+        {
+            var enumDescriptions = new List<string>();
+            Type type = null;
+            foreach (var enumOption in enums)
+            {
+                if (type == null) type = enumOption.GetType();
+                enumDescriptions.Add($"{Convert.ChangeType(enumOption, type.GetEnumUnderlyingType())} = {Enum.GetName(type, enumOption)}，{GetDescription(type, enumOption)}");
+            }
+
+            return $"{Environment.NewLine}{string.Join(Environment.NewLine, enumDescriptions)}";
+        }
+        public static string GetDescription(Type t, object value)
+        {
+            foreach (MemberInfo mInfo in t.GetMembers())
+            {
+                if (mInfo.Name == t.GetEnumName(value))
+                {
+                    foreach (Attribute attr in Attribute.GetCustomAttributes(mInfo))
+                    {
+                        if (attr.GetType() == typeof(DescriptionAttribute))
+                        {
+                            return ((DescriptionAttribute)attr).Description;
+                        }
+                    }
+                }
+            }
+            return string.Empty;
+        }
+    }
+}
